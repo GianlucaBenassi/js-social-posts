a1988cf78f60cf5071e6958c7edecb6a5aa80650
Gianluca Benassi <benox1199@gmail.com>
2021-11-22 14:27:24 +0100

    add post function

--- main.js
+++ main.js
@@ -55,3 +55,62 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+for (let i = 0; i < posts.length; i++) {
+
+    // destructuring posts
+    const {content, media, author: {name}, author: {image}, likes, created} = posts[i];
+
+    addPost(content, media, name, image, likes, created);
+}
+
+
+
+
+// ***** functions
+
+function addPost(content, media, name, image, likes, created) {
+
+    const postContainer = document.getElementById('container');
+
+
+    postContainer.innerHTML += `
+    <div class="post">
+
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${image}" alt="${image}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${name}</div>
+                    <div class="post-meta__time">${created}</div>
+                </div>                    
+            </div>
+        </div>
+
+        <div class="post__text">${content}</div>
+
+        <div class="post__image">
+            <img src="${media}" alt="${media}">
+        </div>
+
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                </div>
+            </div> 
+        </div>
+
+    </div>
+    `;
+
+}
